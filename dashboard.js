3b0b1c00c92afef087171a0cacfef84d8903f309
rhbrands <trollandvee@gmail.com>
2026-01-29 00:22:27 +0100

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDUuzw189X97PKegWApVMTUEY5AJC6F5r8",
+  authDomain: "rearnhub.firebaseapp.com",
+  projectId: "rearnhub",
+  storageBucket: "rearnhub.firebasestorage.app",
+  messagingSenderId: "461077159495",
+  appId: "1:461077159495:web:595412074b4c28de17db62"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Protect page: redirect if not logged in
+onAuthStateChanged(auth, user => {
+  if (user) {
+    document.getElementById("userEmail").textContent = user.email;
+  } else {
+    // Not logged in → redirect to login
+    window.location.href = "login.html";
+  }
+});
+
+// Logout button
+const logoutBtn = document.getElementById("logoutBtn");
+logoutBtn.addEventListener("click", async () => {
+  await signOut(auth);
+  window.location.href = "login.html";
+});
