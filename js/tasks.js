09069909b227f8a2da80f64aa5710c1bff127296
rhbrands <trollandvee@gmail.com>
2026-01-28 11:37:51 +0100

    Update tasks.js

--- js/tasks.js
+++ js/tasks.js
@@ -4,6 +4,21 @@ import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/fi
 const auth = getAuth();
 const db = getFirestore();
 
+// Helper to update balance display
+async function updateBalances(user) {
+  const userRef = doc(db, "users", user.uid);
+  const userDoc = await getDoc(userRef);
+  const data = userDoc.data();
+
+  const taskBalance = data.taskBalance || 0;
+  const refBalance = data.refBalance || 0;
+
+  document.getElementById("taskBalance").innerText = taskBalance;
+  document.getElementById("refBalance").innerText = refBalance;
+  document.getElementById("totalBalance").innerText = taskBalance + refBalance;
+}
+
+// Attach tasks
 function attachTask(buttonId, taskName, points) {
   const btn = document.getElementById(buttonId);
   if (btn) {
@@ -13,17 +28,24 @@ function attachTask(buttonId, taskName, points) {
 
       const userRef = doc(db, "users", user.uid);
       const userDoc = await getDoc(userRef);
-      let currentPoints = userDoc.data().points || 0;
+      let taskBalance = userDoc.data().taskBalance || 0;
 
-      currentPoints += points;
-      await updateDoc(userRef, { points: currentPoints });
-      document.getElementById("points").innerText = currentPoints;
+      taskBalance += points;
+      await updateDoc(userRef, { taskBalance });
 
+      await updateBalances(user);
       alert(`Task "${taskName}" completed! +${points} points`);
     });
   }
 }
 
-// Attach tasks
+// Attach task buttons
 attachTask("task1Btn", "Read Article", 10);
 attachTask("task2Btn", "Watch Video", 15);
+
+// Initialize balances on load
+auth.onAuthStateChanged(async (user) => {
+  if (user) {
+    await updateBalances(user);
+  }
+});
