68b69bb914cee4adc51517ef1ab562aa9871f812
rhbrands <trollandvee@gmail.com>
2026-01-28 23:51:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("RearnHub loaded successfully");
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
-  // Future: check auth state
-  // Future: load user data
-  // Future: task interactions
-});
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUuzw189X97PKegWApVMTUEY5AJC6F5r8",
+  authDomain: "rearnhub.firebaseapp.com",
+  projectId: "rearnhub",
+  storageBucket: "rearnhub.firebasestorage.app",
+  messagingSenderId: "461077159495",
+  appId: "1:461077159495:web:595412074b4c28de17db62"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Test
+console.log("RearnHub loaded");
+console.log("Firebase initialized", app);
